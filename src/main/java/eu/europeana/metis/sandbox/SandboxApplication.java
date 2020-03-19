c2962d70e8412ea551b07c1e52394415551a5fd2
Alejandro <alejandro@apples-MacBook-Pro.local>
2020-03-19 18:19:23 +0100

    Adding changes to generate war instead of jar file

--- src/main/java/eu/europeana/metis/sandbox/SandboxApplication.java
+++ src/main/java/eu/europeana/metis/sandbox/SandboxApplication.java
@@ -2,9 +2,10 @@ package eu.europeana.metis.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SandboxApplication {
+public class SandboxApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(SandboxApplication.class, args);
