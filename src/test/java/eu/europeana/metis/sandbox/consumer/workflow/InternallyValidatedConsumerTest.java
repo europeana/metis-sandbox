4fe89042c8624b02b04911f88dfd26357d0cd2b8
Alejandro <alejandro@apples-MacBook-Pro.local>
2020-04-07 13:16:30 +0200

    Adding test

---
+++ src/test/java/eu/europeana/metis/sandbox/consumer/workflow/InternallyValidatedConsumerTest.java
@@ -0,0 +1,13 @@
+package eu.europeana.metis.sandbox.consumer.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class InternallyValidatedConsumerTest {
+
+  @Test
+  void normalize() {
+  }
+}
