ff05110239365b66e9e7f8a0b815a34366e66fce
Alejandro <alejandro@apples-MacBook-Pro.local>
2020-04-10 10:52:13 +0200

    Adjust exceptions logic

--- src/main/java/eu/europeana/metis/sandbox/common/exception/RecordProcessingException.java
+++ src/main/java/eu/europeana/metis/sandbox/common/exception/RecordProcessingException.java
@@ -2,6 +2,10 @@ package eu.europeana.metis.sandbox.common.exception;
 
 import static java.lang.String.format;
 
+/**
+ * Represents an exception thrown while processing a record <br />
+ * Contains the record id
+ */
 public class RecordProcessingException extends ServiceException {
 
   private static final long serialVersionUID = -3104290194914814439L;
@@ -9,10 +13,17 @@ public class RecordProcessingException extends ServiceException {
   private final String recordId;
 
   public RecordProcessingException(String recordId, Throwable cause) {
-    super(format("Record: %s. Message: %s ", recordId, cause.getMessage()), cause);
+    super(format("Record: %s Message: %s ", recordId, cause.getMessage()), cause);
     this.recordId = recordId;
   }
 
+  /**
+   * Constructor intended to be used by exceptions inheriting from this one. <br />
+   * Gives the ability to provide a custom message
+   * @param message
+   * @param recordId
+   * @param cause
+   */
   protected RecordProcessingException(String message, String recordId, Throwable cause) {
     super(message, cause);
     this.recordId = recordId;
--- src/main/java/eu/europeana/metis/sandbox/common/exception/RecordValidationException.java
+++ src/main/java/eu/europeana/metis/sandbox/common/exception/RecordValidationException.java
@@ -2,6 +2,10 @@ package eu.europeana.metis.sandbox.common.exception;
 
 import static java.lang.String.format;
 
+/**
+ * Represents an exception thrown while validating a record <br />
+ * Contains the record id and the node id
+ */
 public class RecordValidationException extends RecordProcessingException {
 
   private static final long serialVersionUID = -3148276453701960919L;
@@ -9,7 +13,7 @@ public class RecordValidationException extends RecordProcessingException {
   private final String nodeId;
 
   public RecordValidationException(String message, String recordId, String nodeId) {
-    super(format("Record: %s Node: %s. Message: %s ",
+    super(format("Record: %s Node: %s Message: %s ",
         recordId, nodeId, message), recordId, null);
     this.nodeId = nodeId;
   }
