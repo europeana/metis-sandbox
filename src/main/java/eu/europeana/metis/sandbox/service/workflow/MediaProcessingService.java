074707b76fc687050dcb81af340713c8b5fdbec7
Alejandro <alejandro@apples-MacBook-Pro.local>
2020-04-17 17:17:55 +0200

    Fix doc

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/MediaProcessingService.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/MediaProcessingService.java
@@ -9,7 +9,7 @@ public interface MediaProcessingService {
    * Process media of the given record and store it in a persistent storage
    *
    * @param record must not be null
-   * @return TODO check what is returned
+   * @return {@link Record}
    * @throws NullPointerException if record is null
    * @throws RecordProcessingException if record fails at media processing
    */
