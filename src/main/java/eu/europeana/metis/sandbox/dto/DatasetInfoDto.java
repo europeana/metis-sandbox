44986ce6ec1ce3bdb9480d5acd19afa4c6c942ba
Alejandro <ale.180684@gmail.com>
2020-05-06 15:43:07 +0200

    Sonarqube feedback

--- src/main/java/eu/europeana/metis/sandbox/dto/DatasetInfoDto.java
+++ src/main/java/eu/europeana/metis/sandbox/dto/DatasetInfoDto.java
@@ -3,34 +3,27 @@ package eu.europeana.metis.sandbox.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.europeana.metis.sandbox.dto.report.ReportByStepDto;
 import io.swagger.annotations.ApiModel;
+import java.util.Collections;
 import java.util.List;
 
 @ApiModel("DatasetInfo")
 public class DatasetInfoDto {
 
-  private String progress;
+  private final String progress;
 
   @JsonProperty("errors-report")
-  private List<ReportByStepDto> errorsReport;
+  private final List<ReportByStepDto> errorsReport;
 
   public DatasetInfoDto(String progress, List<ReportByStepDto> errorsReport) {
     this.progress = progress;
-    this.errorsReport = errorsReport;
+    this.errorsReport = Collections.unmodifiableList(errorsReport);
   }
 
   public List<ReportByStepDto> getErrorsReport() {
     return errorsReport;
   }
 
-  public void setErrorsReport(List<ReportByStepDto> errorsReport) {
-    this.errorsReport = errorsReport;
-  }
-
   public String getProgress() {
     return progress;
-  }
-
-  public void setProgress(String progress) {
-    this.progress = progress;
   }
 }
--- src/main/java/eu/europeana/metis/sandbox/dto/report/ErrorInfoDto.java
+++ src/main/java/eu/europeana/metis/sandbox/dto/report/ErrorInfoDto.java
@@ -5,25 +5,26 @@ import static java.util.Objects.requireNonNull;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.europeana.metis.sandbox.common.Status;
 import io.swagger.annotations.ApiModel;
+import java.util.Collections;
 import java.util.List;
 
 @ApiModel("ErrorInfo")
 public class ErrorInfoDto {
 
   @JsonProperty("error-message")
-  private String errorMessage;
+  private final String errorMessage;
 
-  private Status status;
+  private final Status status;
 
   @JsonProperty("records")
-  private List<String> recordIds;
+  private final List<String> recordIds;
 
   public ErrorInfoDto(String errorMessage, Status status,
       List<String> recordIds) {
     requireNonNull(errorMessage, "Error message must not be null");
     requireNonNull(recordIds, "Record ids must not be null");
     requireNonNull(status, "Status must not be null");
-    this.recordIds = recordIds;
+    this.recordIds = Collections.unmodifiableList(recordIds);
     this.status = status;
     this.errorMessage = errorMessage;
   }
@@ -32,23 +33,11 @@ public class ErrorInfoDto {
     return recordIds;
   }
 
-  public void setRecordIds(List<String> recordIds) {
-    this.recordIds = recordIds;
-  }
-
   public Status getStatus() {
     return status;
   }
 
-  public void setStatus(Status status) {
-    this.status = status;
-  }
-
   public String getErrorMessage() {
     return errorMessage;
-  }
-
-  public void setErrorMessage(String errorMessage) {
-    this.errorMessage = errorMessage;
   }
 }
