8d728b21dff6c5af3abd78d03443d64ac51acbc0
acerdas84 <acerdas84@users.noreply.github.com>
2020-05-22 18:10:46 +0200

    Feat/met 2733 publish records (#15)
    MET-2733: publish data after indexing in preview

--- src/main/java/eu/europeana/metis/sandbox/config/IndexingConfig.java
+++ src/main/java/eu/europeana/metis/sandbox/config/IndexingConfig.java
@@ -20,48 +20,112 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class IndexingConfig {
 
-  @Value("${sandbox.indexing.mongo.hosts}")
-  private String[] mongoHosts;
+  @Value("${sandbox.preview.mongo.hosts}")
+  private String[] mongoPreviewHosts;
 
-  @Value("${sandbox.indexing.mongo.ports}")
-  private int[] mongoPorts;
+  @Value("${sandbox.preview.mongo.ports}")
+  private int[] mongoPreviewPorts;
 
-  @Value("${sandbox.indexing.mongo.authentication-db:#{null}}")
-  private String mongoAuthenticationDb;
+  @Value("${sandbox.preview.mongo.authentication-db:#{null}}")
+  private String mongoPreviewAuthenticationDb;
 
-  @Value("${sandbox.indexing.mongo.username:#{null}}")
-  private String mongoUsername;
+  @Value("${sandbox.preview.mongo.username:#{null}}")
+  private String mongoPreviewUsername;
 
-  @Value("${sandbox.indexing.mongo.password:#{null}}")
-  private String mongoPassword;
+  @Value("${sandbox.preview.mongo.password:#{null}}")
+  private String mongoPreviewPassword;
 
-  @Value("${sandbox.indexing.mongo.ssl-enable:#{null}}")
-  private Boolean mongoEnableSSL;
+  @Value("${sandbox.preview.mongo.ssl-enable:#{null}}")
+  private Boolean mongoPreviewEnableSSL;
 
-  @Value("${sandbox.indexing.mongo.db}")
-  private String mongoDb;
+  @Value("${sandbox.preview.mongo.db}")
+  private String mongoPreviewDb;
 
-  @Value("${sandbox.indexing.solr.hosts}")
-  private String[] solrHosts;
+  @Value("${sandbox.preview.solr.hosts}")
+  private String[] solrPreviewHosts;
 
-  @Value("${sandbox.indexing.solr.zookeeper.hosts:#{null}}")
-  private String[] zookeeperHosts;
+  @Value("${sandbox.preview.solr.zookeeper.hosts:#{null}}")
+  private String[] zookeeperPreviewHosts;
 
-  @Value("${sandbox.indexing.solr.zookeeper.ports:#{null}}")
-  private int[] zookeeperPorts;
+  @Value("${sandbox.preview.solr.zookeeper.ports:#{null}}")
+  private int[] zookeeperPreviewPorts;
 
-  @Value("${sandbox.indexing.solr.zookeeper.chroot:#{null}}")
-  private String zookeeperChroot;
+  @Value("${sandbox.preview.solr.zookeeper.chroot:#{null}}")
+  private String zookeeperPreviewChroot;
 
-  @Value("${sandbox.indexing.solr.zookeeper.default.collection:#{null}}")
-  private String zookeeperDefaultCollection;
+  @Value("${sandbox.preview.solr.zookeeper.default.collection:#{null}}")
+  private String zookeeperPreviewDefaultCollection;
 
-  @Value("${sandbox.indexing.solr.zookeeper.timeout:#{null}}")
-  private Integer zookeeperTimeoutInSecs;
+  @Value("${sandbox.preview.solr.zookeeper.timeout:#{null}}")
+  private Integer zookeeperPreviewTimeoutInSecs;
+
+  @Value("${sandbox.publish.mongo.hosts}")
+  private String[] mongoPublishHosts;
+
+  @Value("${sandbox.publish.mongo.ports}")
+  private int[] mongoPublishPorts;
+
+  @Value("${sandbox.publish.mongo.authentication-db:#{null}}")
+  private String mongoPublishAuthenticationDb;
+
+  @Value("${sandbox.publish.mongo.username:#{null}}")
+  private String mongoPublishUsername;
+
+  @Value("${sandbox.publish.mongo.password:#{null}}")
+  private String mongoPublishPassword;
+
+  @Value("${sandbox.publish.mongo.ssl-enable:#{null}}")
+  private Boolean mongoPublishEnableSSL;
+
+  @Value("${sandbox.publish.mongo.db}")
+  private String mongoPublishDb;
+
+  @Value("${sandbox.publish.solr.hosts}")
+  private String[] solrPublishHosts;
+
+  @Value("${sandbox.publish.solr.zookeeper.hosts:#{null}}")
+  private String[] zookeeperPublishHosts;
+
+  @Value("${sandbox.publish.solr.zookeeper.ports:#{null}}")
+  private int[] zookeeperPublishPorts;
+
+  @Value("${sandbox.publish.solr.zookeeper.chroot:#{null}}")
+  private String zookeeperPublishChroot;
+
+  @Value("${sandbox.publish.solr.zookeeper.default.collection:#{null}}")
+  private String zookeeperPublishDefaultCollection;
+
+  @Value("${sandbox.publish.solr.zookeeper.timeout:#{null}}")
+  private Integer zookeeperPublishTimeoutInSecs;
 
   @Bean
-  Indexer indexer()
+  Indexer previewIndexer()
       throws URISyntaxException, SetupRelatedIndexingException, IndexerRelatedIndexingException {
+    return getIndexer(mongoPreviewHosts, mongoPreviewPorts, mongoPreviewDb,
+        mongoPreviewAuthenticationDb,
+        mongoPreviewUsername, mongoPreviewPassword, mongoPreviewEnableSSL, solrPreviewHosts,
+        zookeeperPreviewHosts, zookeeperPreviewPorts, zookeeperPreviewChroot,
+        zookeeperPreviewDefaultCollection, zookeeperPreviewTimeoutInSecs);
+  }
+
+  @Bean
+  Indexer publishIndexer()
+      throws URISyntaxException, SetupRelatedIndexingException, IndexerRelatedIndexingException {
+    return getIndexer(mongoPublishHosts, mongoPublishPorts, mongoPublishDb,
+        mongoPublishAuthenticationDb,
+        mongoPublishUsername, mongoPublishPassword, mongoPublishEnableSSL, solrPublishHosts,
+        zookeeperPublishHosts, zookeeperPublishPorts, zookeeperPublishChroot,
+        zookeeperPublishDefaultCollection, zookeeperPublishTimeoutInSecs);
+  }
+
+  //Suppress: Methods should not have too many parameters warning
+  //We are okay with this method to ease configuration
+  @SuppressWarnings("squid:S107")
+  private Indexer getIndexer(String[] mongoHosts, int[] mongoPorts, String mongoDb,
+      String mongoAuthenticationDb, String mongoUsername, String mongoPassword,
+      Boolean mongoEnableSSL, String[] solrHosts, String[] zookeeperHosts, int[] zookeeperPorts,
+      String zookeeperChroot, String zookeeperDefaultCollection, Integer zookeeperTimeoutInSecs)
+      throws SetupRelatedIndexingException, URISyntaxException, IndexerRelatedIndexingException {
     checkArgument(isNotBlank(mongoDb), "Mongo db must be provided");
     checkArgument(isNotEmpty(mongoHosts), "Mongo hosts must be provided ");
     checkArgument(isNotEmpty(mongoPorts), "Mongo ports must be provided ");
@@ -69,13 +133,10 @@ class IndexingConfig {
 
     var settings = new IndexingSettings();
 
-    if (isNull(mongoEnableSSL)) {
-      mongoEnableSSL = Boolean.FALSE;
-    }
-
     // Set the Mongo properties
     settings.getMongoProperties().setAllProperties(mongoHosts, mongoPorts,
-        mongoAuthenticationDb, mongoUsername, mongoPassword, mongoEnableSSL, null);
+        mongoAuthenticationDb, mongoUsername, mongoPassword, Boolean.TRUE.equals(mongoEnableSSL),
+        null);
     settings.setMongoDatabaseName(mongoDb);
 
     // Set Solr properties
