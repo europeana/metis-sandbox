cd74e14a014fef78744348d701bd211c491ca8dd
Alejandro <ale.180684@gmail.com>
2020-05-26 15:25:48 +0200

    Redirect main url to swagger ui

--- src/main/java/eu/europeana/metis/sandbox/config/web/MvcConfig.java
+++ src/main/java/eu/europeana/metis/sandbox/config/web/MvcConfig.java
@@ -2,10 +2,16 @@ package eu.europeana.metis.sandbox.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 class MvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addRedirectViewController("/", "swagger-ui.html");
+  }
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
