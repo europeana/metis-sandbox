258cc0cd8ba1a13326b24f95b5184cf84bab2b65
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-10-13 18:04:34 +0200

    met-3926 replaced ZipService with HarvestService in DatasetController. Implementation of HarvestService (missing remove of method downloadFile which is private in harvesting library) Reverted ZipService, since it's going to be removed in a near future

---
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestService.java
@@ -0,0 +1,33 @@
+package eu.europeana.metis.sandbox.service.workflow;
+
+import eu.europeana.metis.sandbox.common.exception.InvalidZipFileException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+
+public interface  HarvestService {
+
+    /**
+     * Harvest the given file {@link MultipartFile} to a list of byte[], one string per file in the zip
+     * <br/> If file is empty then an empty List will be returned
+     *
+     * @param file zip file containing one or more records
+     * @return List of byte[]
+     * @throws InvalidZipFileException  if file is not valid
+     * @throws IllegalArgumentException if file does not contain any records
+     */
+    List<ByteArrayInputStream> harvest(MultipartFile file);
+
+    /**
+     * Harvest the given file {@link java.net.URL} to a list of byte[], one string per file in the zip
+     * <br/> If file is empty then an empty List will be returned
+     *
+     * @param URL URL for zip file containing one or more records
+     * @return List of byte[]
+     * @throws InvalidZipFileException  if file is not valid
+     * @throws IllegalArgumentException if file does not contain any records
+     */
+    List<ByteArrayInputStream> harvest(String URL);
+}
