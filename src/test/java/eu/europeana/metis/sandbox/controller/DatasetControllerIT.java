2a552b2567a713dceb97519ffca79945b2514261
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-10-18 10:04:28 +0200

    MET-3926 Commented Integration test. Integration test for processURL not working

--- src/test/java/eu/europeana/metis/sandbox/controller/DatasetControllerIT.java
+++ src/test/java/eu/europeana/metis/sandbox/controller/DatasetControllerIT.java
@@ -1,11 +1,5 @@
 package eu.europeana.metis.sandbox.controller;
 
-import static eu.europeana.metis.sandbox.common.locale.Country.ITALY;
-import static eu.europeana.metis.sandbox.common.locale.Language.IT;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import eu.europeana.metis.sandbox.SandboxApplication;
 import eu.europeana.metis.sandbox.common.TestUtils;
 import org.junit.jupiter.api.Test;
@@ -14,37 +8,65 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+
 import java.io.File;
 
+import static eu.europeana.metis.sandbox.common.locale.Country.ITALY;
+import static eu.europeana.metis.sandbox.common.locale.Language.IT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-    classes = SandboxApplication.class)
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = SandboxApplication.class)
 @AutoConfigureMockMvc
 class DatasetControllerIT {
 
-  private final TestUtils testUtils = new TestUtils();
+    private final TestUtils testUtils = new TestUtils();
 
-  @Autowired
-  private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-  @Test
-  public void processDataset_expectStatus_accepted() throws Exception {
+    @Test
+    public void processDatasetFromFile_expectStatus_accepted() throws Exception {
 
-    MockMultipartFile dataset = new MockMultipartFile("dataset", "dataset.txt", "text/plain",
-        testUtils.readFileToBytes("zip"+File.separator+"dataset-valid.zip"));
+        MockMultipartFile dataset = new MockMultipartFile("dataset", "dataset.txt", "text/plain",
+                testUtils.readFileToBytes("zip" + File.separator + "dataset-valid.zip"));
 
-    mvc.perform(multipart("/dataset/{name}/processFile", "my-data-set")
-        .file(dataset)
-        .param("country", ITALY.xmlValue())
-        .param("language", IT.xmlValue())
-                    .param("URL", ""))
-        .andExpect(status().isAccepted());
-  }
+        mvc.perform(multipart("/dataset/{name}/processFile", "my-data-set")
+                        .file(dataset)
+                        .param("country", ITALY.xmlValue())
+                        .param("language", IT.xmlValue()))
+                .andExpect(status().isAccepted());
+    }
 
-  @Test
-  public void retrieveDataset_expectStatus_ok() throws Exception {
-    mvc.perform(get("/dataset/{id}", "1"))
-        .andExpect(status().isOk());
-  }
+//    @Test
+//    public void processDatasetFromURL_expectStatus_accepted() throws Exception {
+//
+//        String url = "src"+File.separator+"zip" + File.separator + "dataset-valid.zip";
+//        String fpath = String.format("src%1$stest%1$sresources%1$szip%1$sdataset-valid.zip", File.separator);
+//
+//        File file = ResourceUtils.getFile("classpath:zip"+File.separator + "dataset-valid.zip");
+//        System.out.println(""+ ResourceUtils.getURL(url).toString());
+//        System.out.println(""+ ResourceUtils.getFile(url));
+//        System.out.println(""+ ResourceUtils.getFile(url).getPath());
+//        System.out.println(""+fpath);
+//
+//        assertTrue(new File(fpath).exists());
+//        assertTrue(file.exists());
+//
+//        mvc.perform(multipart("/dataset/{name}/processURL", "my-data-set")
+//                        .param("country", ITALY.xmlValue())
+//                        .param("language", IT.xmlValue())
+//                        .param("url", fpath))
+//                .andExpect(status().isAccepted());
+//    }
+
+    @Test
+    public void retrieveDataset_expectStatus_ok() throws Exception {
+        mvc.perform(get("/dataset/{id}", "1"))
+                .andExpect(status().isOk());
+    }
 
 }
