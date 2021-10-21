7068c926c7929c053e1755d32bd4d6d6863b8fe0
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-10-21 09:49:46 +0200

    MET-3926 Unified HarvestService to throw/use all the same ServiceException. Added Logger where needed. Use of in project temp folder instead of system temp folder

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestService.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestService.java
@@ -1,32 +1,34 @@
 package eu.europeana.metis.sandbox.service.workflow;
 
+import eu.europeana.metis.harvesting.HarvesterException;
 import eu.europeana.metis.sandbox.common.exception.InvalidZipFileException;
+import eu.europeana.metis.sandbox.common.exception.ServiceException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
 
 
-public interface  HarvestService {
+public interface HarvestService {
 
-    /**
-     * Harvest the given file {@link MultipartFile} to a list of byte[], one string per file in the zip
-     * <br/> If file is empty then an empty List will be returned
-     *
-     * @param file zip file containing one or more records
-     * @return List of byte[]
-     * @throws InvalidZipFileException  if file is not valid
-     * @throws IllegalArgumentException if file does not contain any records
-     */
-    List<ByteArrayInputStream> harvest(MultipartFile file);
+  /**
+   * Harvest the given file {@link MultipartFile} to a list of byte[], one string per file in the
+   * zip <br/> If file is empty then an empty List will be returned
+   *
+   * @param file zip file containing one or more records
+   * @return List of byte[]
+   * @throws ServiceException if file is not valid, error reading file, if records are empty
+   */
+  List<ByteArrayInputStream> harvest(MultipartFile file) throws ServiceException;
 
-    /**
-     * Harvest the given file {@link java.net.URL} to a list of byte[], one string per file in the zip
-     *
-     * @param url URL for zip file containing one or more records
-     * @return List of byte[]
-     * @throws InvalidZipFileException  if file is not valid
-     * @throws IllegalArgumentException if file does not contain any records
-     */
-    List<ByteArrayInputStream> harvest(String url);
+  /**
+   * Harvest the given file {@link java.net.URL} to a list of byte[], one string per file in the
+   * zip
+   *
+   * @param url URL for zip file containing one or more records
+   * @return List of byte[]
+   * @throws ServiceException  if file is not valid
+   * @throws IllegalArgumentException if error harvesting from URL, if records are empty
+   */
+  List<ByteArrayInputStream> harvest(String url) throws ServiceException;
 }
