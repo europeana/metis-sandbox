043eef13b05da168b0c99147000048c25ff49a4f
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-10-27 09:36:56 +0200

    MET-3926 Unified method call to metis-harvesting library to one method only.

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImpl.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImpl.java
@@ -7,6 +7,8 @@ import eu.europeana.metis.harvesting.http.HttpHarvester;
 import eu.europeana.metis.harvesting.http.HttpHarvesterImpl;
 import eu.europeana.metis.harvesting.http.HttpRecordIterator;
 import eu.europeana.metis.sandbox.common.exception.ServiceException;
+
+import java.io.*;
 import java.nio.file.Path;
 import java.util.UUID;
 import org.apache.commons.io.FileUtils;
@@ -17,9 +19,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,69 +30,46 @@ public class HarvestServiceImpl implements HarvestService {
   private static final Logger LOGGER = LoggerFactory.getLogger(HarvestServiceImpl.class);
 
   @Override
-  public List<ByteArrayInputStream> harvest(MultipartFile file) {
-    List<ByteArrayInputStream> records = new ArrayList<>();
-    HttpHarvester harvester = new HttpHarvesterImpl();
-    try {
-      harvester.harvestRecords(file.getInputStream(), CompressedFileExtension.ZIP, entry -> {
-        final byte[] content = entry.getEntryContent().readAllBytes();
-        records.add(new ByteArrayInputStream(content));
-      });
+  public List<ByteArrayInputStream> harvest(MultipartFile file)  {
 
-    } catch (IOException | HarvesterException e) {
+    List<ByteArrayInputStream> records;
+    try {
+      records = harvest(file.getInputStream());
+    } catch (IOException e) {
       throw new ServiceException(e.getMessage(), e);
     }
-
-    if (records.isEmpty()) {
-      throw new ServiceException("Provided file does not contain any records", null);
-    }
-
     return records;
   }
 
   @Override
   public List<ByteArrayInputStream> harvest(String url) {
 
-    Path tempDir = null;
+    List<ByteArrayInputStream> records;
+    try {
+      FileInputStream fis = new FileInputStream(url);
+      records = harvest(fis);
+    } catch (IOException e) {
+      LOGGER.warn(e.getMessage(),e);
+      throw new ServiceException(e.getMessage(), e);
+    }
+    return records;
+  }
+
+  private List<ByteArrayInputStream> harvest(InputStream is){
+
     List<ByteArrayInputStream> records = new ArrayList<>();
     HttpHarvester harvester = new HttpHarvesterImpl();
-
     try {
-      final String prefix = UUID.randomUUID().toString();
-      try {
-        tempDir = Files.createTempDirectory(prefix);
-      } catch (IOException e) {
-        throw new ServiceException(e.getMessage(), e);
-      }
-      // Now perform the harvesting
-      final HttpRecordIterator iterator = harvester.harvestRecords(url, tempDir.toString());
-      List<Pair<Path, Exception>> exception = new ArrayList<>(1);
-      iterator.forEach(path -> {
-        try (InputStream content = Files.newInputStream(path)) {
-          records.add(new ByteArrayInputStream(content.readAllBytes()));
-          return ReportingIteration.IterationResult.CONTINUE;
-        } catch (IOException | RuntimeException e) {
-          exception.add(new ImmutablePair<>(path, e));
-          LOGGER.warn(e.getMessage(), e);
-          return ReportingIteration.IterationResult.TERMINATE;
-        }
+      harvester.harvestRecords(is, CompressedFileExtension.ZIP, entry -> {
+        final byte[] content = entry.getEntryContent().readAllBytes();
+        records.add(new ByteArrayInputStream(content));
       });
-      if (!exception.isEmpty()) {
-        throw new ServiceException("Could not process path " + exception.get(0).getKey() + ".",
-            exception.get(0).getValue());
-      }
+
     } catch (HarvesterException e) {
-      LOGGER.warn(e.getMessage(), e);
-    } finally {
-      // Finally, attempt to delete the files.
-      if (tempDir != null) {
-        try {
-          FileUtils.deleteDirectory(tempDir.toFile());
-        } catch (IOException e) {
-          LOGGER.warn("Could not delete temporary directory.", e);
-        }
-      }
+      LOGGER.warn(e.getMessage(),e);
+      throw new ServiceException(e.getMessage(), e);
     }
+
     if (records.isEmpty()) {
       throw new ServiceException("Provided file does not contain any records", null);
     }
