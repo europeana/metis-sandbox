eae36a90a842395f811fc98ba83a795cf82644c5
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-11-01 12:04:51 +0100

    MET-3926 Corrected Unit tests for HarvestServiceImpl Added corrupt_file.zip for Unit tests

--- src/test/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImplTest.java
+++ src/test/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImplTest.java
@@ -1,86 +1,63 @@
 package eu.europeana.metis.sandbox.service.workflow;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import eu.europeana.metis.sandbox.common.TestUtils;
 import eu.europeana.metis.sandbox.common.exception.ServiceException;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
+import eu.europeana.metis.utils.ZipFileReader;
 import java.io.IOException;
-import java.util.List;
-import org.apache.commons.io.FileUtils;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.multipart.MultipartFile;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.zeroturnaround.zip.ZipException;
 
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class HarvestServiceImplTest {
 
 
-  private final TestUtils testUtils = new TestUtils();
-  @MockBean
-  private HarvestService harvestService;
+  private final ZipFileReader zipfilereader = new ZipFileReader();
+
+  @InjectMocks
+  private HarvestServiceImpl harvestService;
 
   @Test
-  void harvestFromURL_expectSuccess() throws IOException {
+  void harvestService_expectSuccess() throws IOException {
 
-    String url = "zip" + File.separator + "dataset-valid.zip";
+    Path dsPath = Paths.get("src","test","resources","zip","dataset-valid.zip");
+    assertTrue(Files.exists(dsPath));
 
-    var expectedRecords =
-        List.of(new ByteArrayInputStream(testUtils.readFileToBytes(url)));
+    var expectedRecords = zipfilereader.getContentFromZipFile(Files.newInputStream(dsPath));
 
-    when(harvestService.harvest(url)).thenReturn(expectedRecords);
-
-    var result = harvestService.harvest(url);
-
-    assertArrayEquals(expectedRecords.toArray(), result.toArray());
+    var records = harvestService.harvest(dsPath.toString());
+    // In spite records being the same size, they are not in the same order.
+    // So comparing with assertArrayEquals() will output a failed assertion
+    // assertArrayEquals(expectedRecords.toArray(),records.toArray());
+    assertEquals(expectedRecords.size(), records.size());
   }
 
   @Test
-  void harvestFromURL_expectFail()  {
+  void harvestService_expectFailWithServiceException() {
 
-    String url = "zip" + File.separator + "dataset-valid.zip";
+    Path dsPath = Paths.get("src","test","resources","zip","non-existing-file.zip");
+    assertFalse(Files.exists(dsPath));
 
-    when(harvestService.harvest(url)).thenThrow(
-        new ServiceException("Error harvesting records from " + url, new Exception()));
-
-    assertThrows(ServiceException.class, () -> harvestService.harvest(url));
+    assertThrows(ServiceException.class, () -> harvestService.harvest(dsPath.toString()));
   }
 
   @Test
-  void harvestFromFileInputStream_expectSuccess() throws IOException {
+  void harvestService_expectFailWithZipException() {
 
-    File file = FileUtils.getFile("src", "test", "resources", "zip", "dataset-valid.zip");
-    FileInputStream fis = new FileInputStream(file);
-    MultipartFile mfile = new MockMultipartFile("dataset-valid", fis);
+    Path dsPath = Paths.get("src","test","resources","zip","corrupt_file.zip");
+    assertTrue(Files.exists(dsPath));
 
-    var expectedRecords = List.of(new ByteArrayInputStream(mfile.getBytes()));
-
-    when(harvestService.harvest(mfile)).thenReturn(expectedRecords);
-
-    var result = harvestService.harvest(mfile);
-
-    assertArrayEquals(expectedRecords.toArray(), result.toArray());
-  }
-
-  @Test
-  void harvestFromFileInputStream_expectFail() throws IOException {
-
-    File file = FileUtils.getFile("src", "test", "resources", "zip", "dataset-valid.zip");
-    FileInputStream fis = new FileInputStream(file);
-    MultipartFile mfile = new MockMultipartFile("dataset-valid", fis);
-
-    when(harvestService.harvest(mfile)).thenThrow(
-        new ServiceException("Error harvesting records from " + mfile.getName(), new Exception()));
-
-    assertThrows(ServiceException.class, () -> harvestService.harvest(mfile));
+    assertThrows(ZipException.class, () -> harvestService.harvest(dsPath.toString()));
   }
 
 }
