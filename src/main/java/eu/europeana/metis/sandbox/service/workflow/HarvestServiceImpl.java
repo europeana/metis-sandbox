749cd194706d776b53a509891165a417eb778d6e
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-11-01 14:59:02 +0100

    MET-3983 Removed HarvestServiceOaiPmh, unified under the same Service HarvestService

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImpl.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImpl.java
@@ -5,14 +5,20 @@ import eu.europeana.metis.harvesting.ReportingIteration;
 import eu.europeana.metis.harvesting.http.CompressedFileExtension;
 import eu.europeana.metis.harvesting.http.HttpHarvester;
 import eu.europeana.metis.harvesting.http.HttpHarvesterImpl;
-import eu.europeana.metis.harvesting.http.HttpRecordIterator;
+import eu.europeana.metis.harvesting.oaipmh.CloseableOaiClient;
+import eu.europeana.metis.harvesting.oaipmh.OaiHarvest;
+import eu.europeana.metis.harvesting.oaipmh.OaiHarvester;
+import eu.europeana.metis.harvesting.oaipmh.OaiHarvesterImpl;
+import eu.europeana.metis.harvesting.oaipmh.OaiHarvesterImpl.ConnectionClientFactory;
+import eu.europeana.metis.harvesting.oaipmh.OaiRecordHeaderIterator;
+import eu.europeana.metis.harvesting.oaipmh.OaiRepository;
 import eu.europeana.metis.sandbox.common.exception.ServiceException;
-
-import java.io.*;
-import java.nio.file.Path;
-import java.util.UUID;
-import java.util.zip.ZipException;
-import org.apache.commons.io.FileUtils;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
@@ -20,13 +26,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Service
 public class HarvestServiceImpl implements HarvestService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HarvestServiceImpl.class);
+
+  private static ConnectionClientFactory CONNECTION_CLIENT_FACTORY;
 
   @Override
   public List<ByteArrayInputStream> harvest(MultipartFile file) throws ServiceException {
@@ -35,7 +41,7 @@ public class HarvestServiceImpl implements HarvestService {
     try {
       records = harvest(file.getInputStream());
     } catch (IOException e) {
-      throw new ServiceException("Error harvesting records from file "+file.getName(), e);
+      throw new ServiceException("Error harvesting records from file " + file.getName(), e);
     }
     return records;
   }
@@ -43,10 +49,49 @@ public class HarvestServiceImpl implements HarvestService {
   @Override
   public List<ByteArrayInputStream> harvest(String url) throws ServiceException {
     List<ByteArrayInputStream> records;
-    try(FileInputStream fis = new FileInputStream(url)) {
+    try (FileInputStream fis = new FileInputStream(url)) {
       records = harvest(fis);
     } catch (IOException e) {
-      throw new ServiceException("Error harvesting records from "+url, e);
+      throw new ServiceException("Error harvesting records from " + url, e);
+    }
+    return records;
+  }
+
+  @Override
+  public List<ByteArrayInputStream> harvestOaiPmh(String endpoint, String setSpec, String prefix) {
+
+    CloseableOaiClient client = CONNECTION_CLIENT_FACTORY.createConnectionClient(endpoint);
+    OaiHarvester oaiHarvester = new OaiHarvesterImpl(CONNECTION_CLIENT_FACTORY);
+    List<ByteArrayInputStream> records = new ArrayList<>();
+    List<Pair<String, Exception>> exceptions = new ArrayList<>(1);
+
+    try {
+      OaiRecordHeaderIterator recordHeaderIterator =
+          oaiHarvester.harvestRecordHeaders(new OaiHarvest(endpoint, prefix, setSpec));
+
+      recordHeaderIterator.forEach(r -> {
+        OaiRepository oaiRepo = new OaiRepository(endpoint, prefix);
+        try {
+          var rec = oaiHarvester.harvestRecord(oaiRepo, r.getOaiIdentifier());
+          records.add(new ByteArrayInputStream(rec.getRecord().readAllBytes()));
+        } catch (HarvesterException | IOException e) {
+          exceptions.add(new ImmutablePair<>(r.getOaiIdentifier(), e));
+          return ReportingIteration.IterationResult.TERMINATE;
+        }
+        return ReportingIteration.IterationResult.CONTINUE;
+      });
+      if (!exceptions.isEmpty()) {
+        throw new ServiceException("Error harvesting records " + exceptions.get(0).getKey(),
+            exceptions.get(0).getValue());
+      }
+    } catch (HarvesterException e) {
+      throw new ServiceException("Error harvesting records ", e);
+    } finally {
+      try {
+        client.close();
+      } catch (IOException e) {
+        LOGGER.warn("Error closing OAI client ", e);
+      }
     }
     return records;
   }
