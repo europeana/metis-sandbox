e1d7534af63e0f965cb774041ccfa431ed4638e7
Adolfo Peixinho <adolfo.peixinho@europeana.eu>
2021-11-02 18:37:30 +0100

    MET-3926 Added incremental parameter to controller with default value. Changed HarvestServiceImpl to use HarvesterFactory for all implementations File,URL,OAI-MPH Fixed unit tests and integration tests accordingly

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestService.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/HarvestService.java
@@ -1,17 +1,16 @@
 package eu.europeana.metis.sandbox.service.workflow;
 
 import eu.europeana.metis.sandbox.common.exception.ServiceException;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.ByteArrayInputStream;
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 
 public interface HarvestService {
 
   /**
    * Harvest the given file {@link MultipartFile} to a list of byte[], one string per file in the
-   * zip <br/> If file is empty then an empty List will be returned
+   * zip
    *
    * @param file zip file containing one or more records
    * @return List of byte[]
@@ -20,15 +19,26 @@ public interface HarvestService {
   List<ByteArrayInputStream> harvest(MultipartFile file) throws ServiceException;
 
   /**
-   * Harvest the given file {@link java.net.URL} to a list of byte[], one string per file in the
-   * zip
+   * Harvest the given URL {@link String} to a list of byte[], one string per file in the zip
    *
    * @param url URL for zip file containing one or more records
    * @return List of byte[]
-   * @throws ServiceException  if file is not valid
+   * @throws ServiceException if file is not valid, error reading file, if records are empty
    */
   List<ByteArrayInputStream> harvest(String url) throws ServiceException;
 
-  List<ByteArrayInputStream> harvestOaiPmh(String endpoint, String setSpec, String prefix);
+  /**
+   * Harvest the given endpoint {@link String} to a list of byte[], one string per file in the
+   * zip
+   *
+   * @param endpoint for zip file containing one or more records
+   * @param setSpec record specification
+   * @param prefix record prefix
+   * @param incremental Boolean to specify incremental processing
+   * @return List of byte[]
+   * @throws ServiceException if file is not valid, error reading file, if records are empty
+   */
+  List<ByteArrayInputStream> harvest(String endpoint, String setSpec, String prefix,
+      Boolean incremental);
 
 }
--- src/test/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImplTest.java
+++ src/test/java/eu/europeana/metis/sandbox/service/workflow/HarvestServiceImplTest.java
@@ -15,11 +15,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.zeroturnaround.zip.ZipException;
+import org.springframework.mock.web.MockMultipartFile;
 
 
 @ExtendWith(MockitoExtension.class)
 public class HarvestServiceImplTest {
+
 
   private final ZipFileReader zipfilereader = new ZipFileReader();
 
@@ -27,14 +28,15 @@ public class HarvestServiceImplTest {
   private HarvestServiceImpl harvestService;
 
   @Test
-  void harvestService_expectSuccess() throws IOException {
+  void harvestServiceFromURL_expectSuccess() throws IOException {
 
-    Path dataSetPath = Paths.get("src","test","resources","zip","dataset-valid.zip");
+    Path dataSetPath = Paths.get("src", "test", "resources", "zip", "dataset-valid.zip");
     assertTrue(Files.exists(dataSetPath));
 
     var expectedRecords = zipfilereader.getContentFromZipFile(Files.newInputStream(dataSetPath));
 
-    var records = harvestService.harvest(dataSetPath.toString());
+    var records = harvestService.harvest(dataSetPath.toUri().toString());
+
     // In spite records being the same size, they are not in the same order.
     // So comparing with assertArrayEquals() will output a failed assertion
     // assertArrayEquals(expectedRecords.toArray(),records.toArray());
@@ -42,21 +44,40 @@ public class HarvestServiceImplTest {
   }
 
   @Test
-  void harvestService_expectFailWithServiceException() {
+  void harvestServiceFromUploadFile_expectSuccess() throws IOException {
 
-    Path dataSetPath = Paths.get("src","test","resources","zip","non-existing-file.zip");
+    Path dataSetPath = Paths.get("src", "test", "resources", "zip", "dataset-valid.zip");
+
+    assertTrue(Files.exists(dataSetPath));
+
+    MockMultipartFile dataset = new MockMultipartFile("dataset", "dataset.txt", "text/plain",
+        Files.newInputStream(dataSetPath));
+
+    var expectedRecords = zipfilereader.getContentFromZipFile(Files.newInputStream(dataSetPath));
+
+    var records = harvestService.harvest(dataset);
+    // In spite records being the same size, they are not in the same order.
+    // So comparing with assertArrayEquals() will output a failed assertion
+    // assertArrayEquals(expectedRecords.toArray(),records.toArray());
+    assertEquals(expectedRecords.size(), records.size());
+  }
+
+  @Test
+  void harvestService_expectFailNonExistingFile() {
+
+    Path dataSetPath = Paths.get("src", "test", "resources", "zip", "non-existing-file.zip");
     assertFalse(Files.exists(dataSetPath));
 
     assertThrows(ServiceException.class, () -> harvestService.harvest(dataSetPath.toString()));
   }
 
   @Test
-  void harvestService_expectFailWithZipException() {
+  void harvestService_expectFailCorruptFile() {
 
-    Path dataSetPath = Paths.get("src","test","resources","zip","corrupt_file.zip");
+    Path dataSetPath = Paths.get("src", "test", "resources", "zip", "corrupt_file.zip");
     assertTrue(Files.exists(dataSetPath));
 
-    assertThrows(ZipException.class, () -> harvestService.harvest(dataSetPath.toString()));
+    assertThrows(ServiceException.class, () -> harvestService.harvest(dataSetPath.toString()));
   }
 
 }
