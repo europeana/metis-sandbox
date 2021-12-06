5b6e69be7bf04ad9680f8d730f0ebc56ddfa3fa8
Joana Sousa <joana.sousa@europeana.eu>
2021-12-06 15:52:38 +0100

    MET-4038 Changed code to allow multiple queues

--- src/main/java/eu/europeana/metis/sandbox/config/SandboxConfig.java
+++ src/main/java/eu/europeana/metis/sandbox/config/SandboxConfig.java
@@ -46,6 +46,9 @@ class SandboxConfig {
   @Value("${sandbox.rabbitmq.queues.record.transformation.edm.external.queue}")
   private String transformationToEdmExternalQueue;
 
+  @Value("${sandbox.rabbitmq.queues.record.transformation.edm.external.queue}")
+  private String transformationToEdmExternalQueue;
+
   @Value("${sandbox.dataset.creation.threads.core-pool-size}")
   private Integer corePoolSize;
 
