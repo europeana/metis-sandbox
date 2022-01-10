42a71046f2c20a4dbc6340622ced53449ff28ef8
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-01-10 21:07:05 +0100

    MET-4093 add unit test to DatasetGeneratorService to fix bug when xmlRecordProcessorService throws an exception

--- src/main/java/eu/europeana/metis/sandbox/service/dataset/DatasetGeneratorServiceImpl.java
+++ src/main/java/eu/europeana/metis/sandbox/service/dataset/DatasetGeneratorServiceImpl.java
@@ -4,6 +4,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toSet;
 
+import eu.europeana.metis.sandbox.common.exception.RecordParsingException;
 import eu.europeana.metis.sandbox.common.locale.Country;
 import eu.europeana.metis.sandbox.common.locale.Language;
 import eu.europeana.metis.sandbox.domain.Dataset;
@@ -11,6 +12,8 @@ import eu.europeana.metis.sandbox.domain.Record;
 import eu.europeana.metis.sandbox.service.util.XmlRecordProcessorService;
 import java.io.ByteArrayInputStream;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,34 +21,56 @@ class DatasetGeneratorServiceImpl implements DatasetGeneratorService {
 
   private final XmlRecordProcessorService xmlRecordProcessorService;
 
-  public DatasetGeneratorServiceImpl(
-      XmlRecordProcessorService xmlRecordProcessorService) {
+  public DatasetGeneratorServiceImpl(XmlRecordProcessorService xmlRecordProcessorService) {
     this.xmlRecordProcessorService = xmlRecordProcessorService;
   }
 
   @Override
-  public Dataset generate(String id, String name, Country country, Language language,
-      List<ByteArrayInputStream> records) {
+  public Dataset generate(
+      final String id,
+      final String name,
+      final Country country,
+      final Language language,
+      final List<ByteArrayInputStream> records) {
     requireNonNull(name, "Dataset name must not be null");
     requireNonNull(country, "Country must not be null");
     requireNonNull(language, "Language must not be null");
     requireNonNull(records, "Records must not be null");
     checkArgument(!records.isEmpty(), "Records must not be empty");
 
-    var totalRecords = records.size();
-    var recordObjectList = records.stream()
-        .map(ByteArrayInputStream::readAllBytes)
-        .map(record -> Record.builder()
-            .recordId(xmlRecordProcessorService.getRecordId(record))
-            .datasetId(id)
-            .datasetName(name)
-            .country(country)
-            .language(language)
-            .content(record)
-            .build())
-        .collect(toSet());
+    int totalRecords = records.size();
+    Set<Record> recordObjectList =
+        records.stream()
+            .map(ByteArrayInputStream::readAllBytes)
+            .map(
+                recordItem ->
+                    getOptionalRecordFromProcessorService(id, name, country, language, recordItem))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(toSet());
 
     // recordObjectList will remove duplicate records based on Record equals
     return new Dataset(id, recordObjectList, totalRecords - recordObjectList.size());
+  }
+
+  private Optional<Record> getOptionalRecordFromProcessorService(
+      final String id,
+      final String name,
+      final Country country,
+      final Language language,
+      final byte[] recordData) {
+    try {
+      return Optional.of(
+          Record.builder()
+              .recordId(xmlRecordProcessorService.getRecordId(recordData))
+              .datasetId(id)
+              .datasetName(name)
+              .country(country)
+              .language(language)
+              .content(recordData)
+              .build());
+    } catch (RecordParsingException recordParsingException) {
+      return Optional.empty();
+    }
   }
 }
--- src/test/java/eu/europeana/metis/sandbox/service/dataset/DatasetGeneratorServiceImplTest.java
+++ src/test/java/eu/europeana/metis/sandbox/service/dataset/DatasetGeneratorServiceImplTest.java
@@ -5,8 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import eu.europeana.metis.sandbox.common.exception.RecordParsingException;
 import eu.europeana.metis.sandbox.common.locale.Country;
 import eu.europeana.metis.sandbox.common.locale.Language;
+import eu.europeana.metis.sandbox.domain.Dataset;
 import eu.europeana.metis.sandbox.service.util.XmlRecordProcessorService;
 import java.io.ByteArrayInputStream;
 import java.util.List;
@@ -19,36 +21,70 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class DatasetGeneratorServiceImplTest {
 
-  @Mock
-  private XmlRecordProcessorService xmlRecordProcessorService;
+  @Mock private XmlRecordProcessorService xmlRecordProcessorService;
 
-  @InjectMocks
-  private DatasetGeneratorServiceImpl generator;
+  @InjectMocks private DatasetGeneratorServiceImpl generator;
 
   @Test
   void generate_expectSuccess() {
+    when(xmlRecordProcessorService.getRecordId(any(byte[].class))).thenReturn("1").thenReturn("2");
 
-    when(xmlRecordProcessorService.getRecordId(any(byte[].class)))
-        .thenReturn("1")
-        .thenReturn("2");
-
-    var dataset = generator
-        .generate("1", "name", Country.ITALY, Language.IT,
-            List.of(new ByteArrayInputStream("record1".getBytes()),
+    Dataset dataset =
+        generator.generate(
+            "1",
+            "name",
+            Country.ITALY,
+            Language.IT,
+            List.of(
+                new ByteArrayInputStream("record1".getBytes()),
                 new ByteArrayInputStream("records".getBytes())));
 
     assertEquals(2, dataset.getRecords().size());
   }
 
   @Test
-  void generateWithDuplicateRecord_expectSuccess() {
+  void generate_inCaseOfInvalidRecords_expectSuccess() {
 
-    when(xmlRecordProcessorService.getRecordId(any(byte[].class)))
-        .thenReturn("1");
+    when(xmlRecordProcessorService.getRecordId("record1".getBytes()))
+        .thenThrow(RecordParsingException.class);
+    when(xmlRecordProcessorService.getRecordId("record2".getBytes()))
+        .thenReturn("2");
+    when(xmlRecordProcessorService.getRecordId("record3".getBytes()))
+        .thenThrow(RecordParsingException.class);
+    when(xmlRecordProcessorService.getRecordId("record4".getBytes()))
+        .thenThrow(RecordParsingException.class);
+    when(xmlRecordProcessorService.getRecordId("record5".getBytes()))
+        .thenReturn("5");
 
-    var dataset = generator
-        .generate("1", "name", Country.ITALY, Language.IT,
-            List.of(new ByteArrayInputStream("record1".getBytes()),
+    Dataset dataset =
+        generator.generate(
+            "1",
+            "name",
+            Country.ITALY,
+            Language.IT,
+            List.of(
+                new ByteArrayInputStream("record1".getBytes()),
+                new ByteArrayInputStream("record2".getBytes()),
+                new ByteArrayInputStream("record3".getBytes()),
+                new ByteArrayInputStream("record4".getBytes()),
+                new ByteArrayInputStream("record5".getBytes())));
+
+    assertEquals(2, dataset.getRecords().size());
+  }
+
+  @Test
+  void generate_withDuplicateRecord_expectSuccess() {
+
+    when(xmlRecordProcessorService.getRecordId(any(byte[].class))).thenReturn("1");
+
+    Dataset dataset =
+        generator.generate(
+            "1",
+            "name",
+            Country.ITALY,
+            Language.IT,
+            List.of(
+                new ByteArrayInputStream("record1".getBytes()),
                 new ByteArrayInputStream("records".getBytes())));
 
     assertEquals(1, dataset.getRecords().size());
@@ -56,7 +92,8 @@ class DatasetGeneratorServiceImplTest {
 
   @Test
   void generate_emptyRecords_expectFail() {
-    assertThrows(IllegalArgumentException.class, () -> generator
-        .generate("1", "name", Country.ITALY, Language.IT, List.of()));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> generator.generate("1", "name", Country.ITALY, Language.IT, List.of()));
   }
 }
