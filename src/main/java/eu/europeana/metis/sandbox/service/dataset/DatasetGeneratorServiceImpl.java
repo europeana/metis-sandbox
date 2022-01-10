23aa6e7dea7ed7b0235d21c2e8d61a3568bc8ec5
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-01-10 21:07:05 +0100

    MET-4093 add unit test to DatasetGeneratorService to fix bug when xmlRecordProcessorService throws an exception

--- src/main/java/eu/europeana/metis/sandbox/service/dataset/DatasetGeneratorServiceImpl.java
+++ src/main/java/eu/europeana/metis/sandbox/service/dataset/DatasetGeneratorServiceImpl.java
@@ -4,6 +4,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toSet;
 
+import eu.europeana.metis.sandbox.common.exception.RecordParsingException;
 import eu.europeana.metis.sandbox.common.locale.Country;
 import eu.europeana.metis.sandbox.common.locale.Language;
 import eu.europeana.metis.sandbox.domain.Dataset;
@@ -12,6 +13,8 @@ import eu.europeana.metis.sandbox.service.util.XmlRecordProcessorService;
 import eu.europeana.metis.transformation.service.EuropeanaIdCreator;
 import java.io.ByteArrayInputStream;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,37 +22,58 @@ class DatasetGeneratorServiceImpl implements DatasetGeneratorService {
 
   private final XmlRecordProcessorService xmlRecordProcessorService;
 
-  public DatasetGeneratorServiceImpl(
-      XmlRecordProcessorService xmlRecordProcessorService) {
+  public DatasetGeneratorServiceImpl(XmlRecordProcessorService xmlRecordProcessorService) {
     this.xmlRecordProcessorService = xmlRecordProcessorService;
   }
 
   @Override
-  public Dataset generate(String datasetId, String name, Country country, Language language,
-      List<ByteArrayInputStream> records) {
+  public Dataset generate(
+      final String datasetId,
+      final String name,
+      final Country country,
+      final Language language,
+      final List<ByteArrayInputStream> records) {
     requireNonNull(name, "Dataset name must not be null");
     requireNonNull(country, "Country must not be null");
     requireNonNull(language, "Language must not be null");
     requireNonNull(records, "Records must not be null");
     checkArgument(!records.isEmpty(), "Records must not be empty");
 
-    var recordObjectList = records.stream()
-        .map(ByteArrayInputStream::readAllBytes)
-        .map(record -> {
-          var recordId = xmlRecordProcessorService.getRecordId(record);
-          return Record.builder()
+    Set<Record> recordObjectList =
+        records.stream()
+            .map(ByteArrayInputStream::readAllBytes)
+            .map(
+                recordItem ->
+                    getOptionalRecordFromProcessorService(
+                        datasetId, name, country, language, recordItem))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(toSet());
+
+    // recordObjectList will remove duplicate records based on Record equals
+    return new Dataset(datasetId, recordObjectList, records.size() - recordObjectList.size());
+  }
+
+  private Optional<Record> getOptionalRecordFromProcessorService(
+      final String datasetId,
+      final String name,
+      final Country country,
+      final Language language,
+      final byte[] recordData) {
+    try {
+      var recordId = xmlRecordProcessorService.getRecordId(recordData);
+      return Optional.of(
+          Record.builder()
               .recordId(recordId)
               .europeanaId(EuropeanaIdCreator.constructEuropeanaIdString(recordId, datasetId))
               .datasetId(datasetId)
               .datasetName(name)
               .country(country)
               .language(language)
-              .content(record)
-              .build();
-        })
-        .collect(toSet());
-
-    // recordObjectList will remove duplicate records based on Record equals
-    return new Dataset(datasetId, recordObjectList, records.size() - recordObjectList.size());
+              .content(recordData)
+              .build());
+    } catch (RecordParsingException recordParsingException) {
+      return Optional.empty();
+    }
   }
 }
