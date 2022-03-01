59a03f2e8d303eb482e388fdad4a9df480390733
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-03-01 14:16:28 +0100

    MET-4183 catch queue exception and log it when occurs

--- src/main/java/eu/europeana/metis/sandbox/executor/workflow/CloseExecutor.java
+++ src/main/java/eu/europeana/metis/sandbox/executor/workflow/CloseExecutor.java
@@ -4,6 +4,8 @@ import eu.europeana.metis.sandbox.common.Status;
 import eu.europeana.metis.sandbox.common.Step;
 import eu.europeana.metis.sandbox.domain.Event;
 import eu.europeana.metis.sandbox.domain.RecordInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 @Component
 class CloseExecutor {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(CloseExecutor.class);
   private final AmqpTemplate amqpTemplate;
 
   @Value("${sandbox.rabbitmq.routing-key.closed}")
@@ -31,8 +34,11 @@ class CloseExecutor {
     if (input.getStatus() == Status.FAIL) {
       return;
     }
-
-    Event output = new Event(new RecordInfo(input.getBody()), Step.CLOSE, Status.SUCCESS);
-    amqpTemplate.convertAndSend(routingKey, output);
+    try {
+      Event output = new Event(new RecordInfo(input.getBody()), Step.CLOSE, Status.SUCCESS);
+      amqpTemplate.convertAndSend(routingKey, output);
+    } catch (RuntimeException closeException) {
+      LOGGER.error("Close executor error", closeException);
+    }
   }
 }
