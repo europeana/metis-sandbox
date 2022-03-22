dcb45ba1dd5f0b7ca566410e837f16e68611bbf8
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-03-22 15:44:49 +0100

    MET-4357 remove unused import

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/MediaProcessingServiceImpl.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/MediaProcessingServiceImpl.java
@@ -24,7 +24,6 @@ import java.util.List;
 import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
