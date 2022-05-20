84c32ba9a646068c14fe126284efd2d37ee945be
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2022-05-20 17:44:24 +0200

    MET-4458 Add test InternalValidationServiceImplTest

--- src/test/java/eu/europeana/metis/sandbox/service/workflow/InternalValidationServiceImplTest.java
+++ src/test/java/eu/europeana/metis/sandbox/service/workflow/InternalValidationServiceImplTest.java
@@ -1,5 +1,6 @@
 package eu.europeana.metis.sandbox.service.workflow;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -7,6 +8,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import eu.europeana.metis.sandbox.common.Step;
@@ -25,26 +27,32 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = InternalValidationServiceImpl.class, properties = "clean-cache-interval:* * * * * ?")
 class InternalValidationServiceImplTest {
 
   private static final String SCHEMA = "EDM-INTERNAL";
 
-  @Mock
+  @MockBean
   private ValidationExecutionService validationExecutionService;
 
-  @Mock
+  @MockBean
   private PatternAnalysisService<Step, ExecutionPoint> patternAnalysisService;
 
-  @Mock
+  @MockBean
   private ExecutionPointService executionPointService;
 
-  @InjectMocks
+  @Autowired
   private InternalValidationServiceImpl service;
+
+  @Autowired
+  private ScheduledAnnotationBeanPostProcessor postProcessor;
 
   @Test
   void validate_expectSuccess() throws PatternAnalysisException {
@@ -84,6 +92,30 @@ class InternalValidationServiceImplTest {
         .thenReturn(validationResult);
 
     assertThrows(RecordValidationException.class, () -> service.validate(record));
+  }
+
+  @Test
+  void validate_pattern_analysis_fail_but_successful_return() throws PatternAnalysisException {
+    var record = Record.builder().recordId(1L)
+                       .content("".getBytes()).language(Language.IT).country(Country.ITALY)
+                       .datasetName("").datasetId("1").build();
+
+    var validationResult = new ValidationResult();
+    validationResult.setSuccess(true);
+    validationResult.setRecordId("1");
+
+    when(validationExecutionService
+        .singleValidation(eq(SCHEMA), isNull(), isNull(), any(InputStream.class)))
+        .thenReturn(validationResult);
+    when(executionPointService.getExecutionPoint("1", Step.VALIDATE_INTERNAL.toString())).thenReturn(Optional.empty());
+    when(patternAnalysisService.initializePatternAnalysisExecution(anyString(), any(Step.class),
+        any(LocalDateTime.class))).thenReturn(new ExecutionPoint());
+    doThrow(new PatternAnalysisException("Error", null)).when(patternAnalysisService)
+                                                        .generateRecordPatternAnalysis(any(ExecutionPoint.class), anyString());
+
+    var result = assertDoesNotThrow(() -> service.validate(record));
+    assertEquals(record, result.getRecord());
+
   }
 
   @Test
