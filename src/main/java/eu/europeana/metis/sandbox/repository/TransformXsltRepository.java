9ceb610a4695328fcb8f2eebc53337d386233213
Adolfo Peixinho <91942157+adolfopeixinho@users.noreply.github.com>
2022-06-24 14:04:24 +0200

    MET-4599 changed implementation of XsltUrlUpdateServiceImpl (#82)
    * MET-4599 changed implementation of XsltUrlUpdateServiceImpl. Fixed tests accordingly
    
    * MET-4599 second review
    
    * MET-4599 third review
    
    * MET-4599 code cleanup, renaming of tests

--- src/main/java/eu/europeana/metis/sandbox/repository/TransformXsltRepository.java
+++ src/main/java/eu/europeana/metis/sandbox/repository/TransformXsltRepository.java
@@ -1,8 +1,9 @@
 package eu.europeana.metis.sandbox.repository;
 
 import eu.europeana.metis.sandbox.entity.TransformXsltEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransformXsltRepository extends JpaRepository<TransformXsltEntity, Integer> {
-
+  Optional<TransformXsltEntity> findFirstByIdIsNotNullOrderByIdAsc();
 }
