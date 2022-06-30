0abfdd8360aa20558e971731d5288d5dae59ead7
Joana Sousa <joana.sousa@europeana.eu>
2022-06-30 14:50:58 +0200

    MET-4553 Made first integration test from datasetcontroller

--- src/test/java/eu/europeana/metis/sandbox/controller/DatasetControllerIT.java
+++ src/test/java/eu/europeana/metis/sandbox/controller/DatasetControllerIT.java
@@ -2,7 +2,7 @@ package eu.europeana.metis.sandbox.controller;
 
 import static eu.europeana.metis.sandbox.common.locale.Country.ITALY;
 import static eu.europeana.metis.sandbox.common.locale.Language.IT;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -15,41 +15,78 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Disabled;
+
+import eu.europeana.metis.sandbox.test.utils.PostgresContainerInitializerIT;
+import eu.europeana.metis.sandbox.test.utils.RabbitMQContainerInitializerIT;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     classes = SandboxApplication.class)
 @AutoConfigureMockMvc
-@Disabled
 class DatasetControllerIT {
 
   private final TestUtils testUtils = new TestUtils();
+  private final TestRestTemplate testRestTemplate = new TestRestTemplate();
 
   @Autowired
   private MockMvc mvc;
 
+  @Value("${local.server.port}")
+  private int port;
+
+  @DynamicPropertySource
+  public static void dynamicProperties(DynamicPropertyRegistry registry) {
+    PostgresContainerInitializerIT.dynamicProperties(registry);
+    PostgresContainerInitializerIT.runScripts(List.of("database/schema_drop.sql", "database/schema.sql"));
+    RabbitMQContainerInitializerIT.properties(registry);
+  }
+
+  private String getBaseUrl() {
+    return "http://localhost:" + port;
+  }
+
   @Test
-  public void harvestDatasetWithFile_expectStatus_accepted() throws Exception {
+  public void harvestDatasetWithFile_expectStatus_accepted() {
+    FileSystemResource dataset = new FileSystemResource(
+            "src" + File.separator + "test" + File.separator + "resources" + File.separator + "zip" +
+                    File.separator + "dataset-valid.zip");
 
-    MockMultipartFile dataset = new MockMultipartFile("dataset", "dataset.txt", "text/plain",
-        testUtils.readFileToBytes("zip" + File.separator + "dataset-valid.zip"));
 
-    mvc.perform(multipart("/dataset/{name}/harvestByFile", "my-data-set")
-            .file(dataset)
-            .param("country", ITALY.xmlValue())
-            .param("language", IT.xmlValue()))
-        .andExpect(status().isAccepted());
+    HttpHeaders requestHeaders = new HttpHeaders();
+    requestHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+    MultiValueMap<String, Object> body
+            = new LinkedMultiValueMap<>();
+    body.add("dataset", dataset);
+    body.add("country", ITALY.xmlValue());
+    body.add("language", IT.xmlValue());
+
+
+    ResponseEntity<String> response =
+            testRestTemplate.postForEntity(getBaseUrl() + "/dataset/{name}/harvestByFile",
+                    new HttpEntity<>(body, requestHeaders), String.class, "testDataset");
+    assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+    assertNotNull(response.getBody());
+    assertTrue(response.getBody().contains("\"dataset-id\":\"1\""));
+
   }
 
   @Test
