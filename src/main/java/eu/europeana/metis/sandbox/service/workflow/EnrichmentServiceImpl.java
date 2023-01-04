2a892c36a6cdf0ef2672898557b09773fcdcef55
Joana Sousa <joana.sousa@europeana.eu>
2023-01-04 12:02:09 +0100

    MET-2018_MET-4957 Code review changes

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/EnrichmentServiceImpl.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/EnrichmentServiceImpl.java
@@ -11,6 +11,7 @@ import eu.europeana.metis.sandbox.common.exception.ServiceException;
 import eu.europeana.metis.sandbox.domain.Record;
 import eu.europeana.metis.sandbox.domain.RecordError;
 import eu.europeana.metis.sandbox.domain.RecordInfo;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -34,33 +35,31 @@ class EnrichmentServiceImpl implements EnrichmentService {
 
         List<RecordError> recordErrors = new LinkedList<>();
         ProcessedResult<byte[]> result;
-        try {
-            result = enrichmentWorker.process(record.getContentInputStream());
-            Set<Report> reports = result.getReport();
 
-            if (result.getRecordStatus().equals(ProcessedResult.RecordStatus.STOP)) {
-                Optional<Report> report = reports.stream().filter(rep -> Objects.equals(rep.getMessageType(), Type.ERROR)).findFirst();
-                throw new RecordProcessingException(record.getProviderId(), handleRecordStopException(report));
-            }
+        result = enrichmentWorker.process(record.getContentInputStream());
+        Set<Report> reports = result.getReport();
 
-            reports.stream().filter(report -> report.getMessageType().equals(Type.WARN))
-                    .forEach(report -> recordErrors.add(new RecordError(new RecordProcessingException(record.getProviderId(),
-                            new ServiceException(handleErrorMessage(report), null)))));
-
-        } catch (Exception e) {
-            throw new RecordProcessingException(record.getProviderId(), e);
+        if (result.getRecordStatus().equals(ProcessedResult.RecordStatus.STOP)) {
+            handleRecordStopException(reports, record.getProviderId());
         }
+
+        reports.stream().filter(report -> Objects.equals(report.getMessageType(), Type.WARN))
+                .forEach(report -> recordErrors.add(new RecordError(new RecordProcessingException(record.getProviderId(),
+                        new ServiceException(createErrorMessage(report), null)))));
 
         return new RecordInfo(Record.from(record, result.getProcessedRecord()), recordErrors);
     }
 
-    private String handleErrorMessage(Report report) {
-        return String.format("%s Value: %s", report.getMessage() ,report.getValue());
+    private String createErrorMessage(Report report) {
+        return String.format("%s Value: %s", report.getMessage(), report.getValue());
     }
 
-    private Exception handleRecordStopException(Optional<Report> report) {
-        return report.isPresent() ? new ServiceException(handleErrorMessage(report.get()),
-                new Exception("StackTrace: " + report.get().getStackTrace())) :
-                new ServiceException("Something went wrong when requesting report.", null);
+    private void handleRecordStopException(Set<Report> reports, String providerId) {
+        Optional<Report> report = reports.stream().filter(rep -> Objects.equals(rep.getMessageType(), Type.ERROR)).findFirst();
+        if (report.isPresent()) {
+            throw new RecordProcessingException(providerId, new ServiceException(createErrorMessage(report.get()), null));
+        } else {
+            throw new RecordProcessingException(providerId, new ServiceException("Something went wrong when requesting report.", null));
+        }
     }
 }
