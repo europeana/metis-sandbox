d16ed52766f1daeeb72988b8fce523b0839f181f
Jorge Ortiz <jorge.ortiz@europeana.eu>
2023-01-26 20:23:50 +0100

    MET-4506 update drop integration db script

--- src/main/resources/database/schema_lockrepository_drop.sql
+++ src/main/resources/database/schema_lockrepository_drop.sql
@@ -1,10 +1,11 @@
 BEGIN;
+-- SET search_path TO integration;
 -- DROP TABLE IF EXISTS int_message
 -- DROP TABLE IF EXISTS int_group_to_message
 -- DROP TABLE IF EXISTS int_message_group
 -- DROP TABLE IF EXISTS int_lock
 -- DROP TABLE IF EXISTS int_channel_message
 -- DROP TABLE IF EXISTS int_metadata_store
--- DROP TABLE IF EXISTS
+-- DROP SEQUENCE IF EXISTS int_message_seq
 DROP SCHEMA IF EXISTS integration CASCADE;
 COMMIT;
