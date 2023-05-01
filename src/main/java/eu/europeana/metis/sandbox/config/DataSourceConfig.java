a4203755ae18331dd8724c59af533ab754c40a6b
Jorge Ortiz <jorge.ortiz@europeana.eu>
2023-05-01 19:13:21 +0200

    MET-5231 add configuration to use the proper exception in spring integration

--- src/main/java/eu/europeana/metis/sandbox/config/DataSourceConfig.java
+++ src/main/java/eu/europeana/metis/sandbox/config/DataSourceConfig.java
@@ -2,13 +2,21 @@ package eu.europeana.metis.sandbox.config;
 
 import eu.europeana.metis.utils.CustomTruststoreAppender;
 import eu.europeana.metis.utils.CustomTruststoreAppender.TrustStoreConfigurationException;
-import javax.sql.DataSource;
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.TransactionException;
+import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.transaction.support.DefaultTransactionStatus;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 /**
  * Configures the datasource, ensures that the custom trust store is appended before creating the
@@ -17,23 +25,45 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class DataSourceConfig {
 
-  @Value("${sandbox.truststore.path}")
-  private String trustStorePath;
+    @Value("${sandbox.truststore.path}")
+    private String trustStorePath;
 
-  @Value("${sandbox.truststore.password}")
-  private String trustStorePassword;
+    @Value("${sandbox.truststore.password}")
+    private String trustStorePassword;
 
-  @Bean
-  @ConfigurationProperties(prefix = "sandbox.datasource")
-  public DataSource getDataSource() throws TrustStoreConfigurationException {
-    appendCustomTrustStore();
-    return DataSourceBuilder.create().build();
-  }
-
-  private void appendCustomTrustStore()
-      throws TrustStoreConfigurationException {
-    if (StringUtils.isNotEmpty(trustStorePath) && StringUtils.isNotEmpty(trustStorePassword)) {
-      CustomTruststoreAppender.appendCustomTrustoreToDefault(trustStorePath, trustStorePassword);
+    @Bean
+    @ConfigurationProperties(prefix = "sandbox.datasource")
+    public DataSource getDataSource() throws TrustStoreConfigurationException {
+        appendCustomTrustStore();
+        return DataSourceBuilder.create().build();
     }
-  }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory) {
+            @Override
+            protected void doCommit(DefaultTransactionStatus status) {
+                try {
+                    super.doCommit(status);
+                } catch (JpaSystemException jpaSystemException) {
+                    Throwable firstLevelThrowable = jpaSystemException.getCause();
+                    if (firstLevelThrowable instanceof TransactionException) {
+                        Throwable secondLevelThrowable = firstLevelThrowable.getCause();
+                        if (secondLevelThrowable instanceof PSQLException
+                                && "40001".equals(((PSQLException) secondLevelThrowable).getSQLState())) {
+                            throw new TransactionSystemException(firstLevelThrowable.getMessage(), firstLevelThrowable);
+                        }
+                    }
+                    throw jpaSystemException;
+                }
+            }
+        };
+    }
+
+    private void appendCustomTrustStore()
+            throws TrustStoreConfigurationException {
+        if (StringUtils.isNotEmpty(trustStorePath) && StringUtils.isNotEmpty(trustStorePassword)) {
+            CustomTruststoreAppender.appendCustomTrustoreToDefault(trustStorePath, trustStorePassword);
+        }
+    }
 }
