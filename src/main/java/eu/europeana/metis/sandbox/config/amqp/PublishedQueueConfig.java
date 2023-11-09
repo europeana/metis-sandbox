713c6dce72971512e03042c2ce8c42f27fcaaed9
Simon Tzanakis <simon.tzanakis@europeana.eu>
2023-11-09 10:43:14 +0100

    bug/MET-5057: Fix configuration naming and values inconsistencies (#144)
    * MET-5057: Fix configuration naming and values inconsistencies
    
    * MET-5057: Further fixes on rabbitmq listeners configuration setup
    
    * MET-5057: Process review

---
+++ src/main/java/eu/europeana/metis/sandbox/config/amqp/PublishedQueueConfig.java
@@ -0,0 +1,49 @@
+package eu.europeana.metis.sandbox.config.amqp;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * The type Published queue configuration.
+ */
+@Configuration
+public class PublishedQueueConfig extends QueueConsumerConfig {
+
+    @Value("${sandbox.rabbitmq.queues.record.published.concurrency}")
+    private int concurrentConsumers;
+
+    @Value("${sandbox.rabbitmq.queues.record.published.max-concurrency}")
+    private int maxConsumers;
+
+    @Value("${sandbox.rabbitmq.queues.record.published.prefetch}")
+    private int messagePrefetchCount;
+
+    /**
+     * Instantiates a new Published queue configuration.
+     *
+     * @param messageConverter the message converter
+     */
+    public PublishedQueueConfig(MessageConverter messageConverter) {
+        super(messageConverter);
+    }
+
+    /**
+     * Closing factory simple rabbit listener container factory.
+     *
+     * @param configurer        the configurer
+     * @param connectionFactory the connection factory
+     * @return the simple rabbit listener container factory
+     */
+    @Bean
+    SimpleRabbitListenerContainerFactory closingFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer,
+            ConnectionFactory connectionFactory) {
+        return getSimpleRabbitListenerContainerFactory(configurer, connectionFactory, concurrentConsumers, maxConsumers, messagePrefetchCount);
+    }
+}
---
+++ src/main/java/eu/europeana/metis/sandbox/config/amqp/QueueConsumerConfig.java
@@ -0,0 +1,49 @@
+package eu.europeana.metis.sandbox.config.amqp;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The type Queue consumer configuration for workflow listeners.
+ * Every listener has a {@link SimpleRabbitListenerContainerFactory}.
+ * <br /> If changes like increasing concurrency for a listener are needed,
+ * every subclass can be customized by using the SimpleRabbitListenerContainerFactory.
+ */
+@Configuration
+public abstract class QueueConsumerConfig {
+    private final MessageConverter messageConverter;
+
+    /**
+     * Instantiates a new Queue consumer configuration.
+     *
+     * @param messageConverter the message converter
+     */
+    public QueueConsumerConfig(MessageConverter messageConverter) {
+        this.messageConverter = messageConverter;
+    }
+
+    /**
+     * Gets simple rabbit listener container factory.
+     *
+     * @param configurer                  the configurer
+     * @param connectionFactory           the connection factory
+     * @param concurrentQueueConsumers    the concurrent queue consumers
+     * @param maxConcurrentQueueConsumers the maximum concurrent queue consumers
+     * @param messagePrefetchCount        the message prefetch count
+     * @return the simple rabbit listener container factory
+     */
+    protected SimpleRabbitListenerContainerFactory getSimpleRabbitListenerContainerFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer, ConnectionFactory connectionFactory,
+            Integer concurrentQueueConsumers, Integer maxConcurrentQueueConsumers, Integer messagePrefetchCount) {
+        var factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setConcurrentConsumers(concurrentQueueConsumers);
+        factory.setMaxConcurrentConsumers(maxConcurrentQueueConsumers);
+        factory.setPrefetchCount(messagePrefetchCount);
+        factory.setMessageConverter(messageConverter);
+        return factory;
+    }
+}
