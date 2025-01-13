5175439bb68bc76250eed7541264ba66f0c332df
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-01-13 15:30:25 +0100

    MET-6248 add localstack s3 instead of minio

--- src/main/java/eu/europeana/metis/sandbox/config/S3Config.java
+++ src/main/java/eu/europeana/metis/sandbox/config/S3Config.java
@@ -1,7 +1,5 @@
 package eu.europeana.metis.sandbox.config;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -10,7 +8,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import eu.europeana.metis.sandbox.domain.Bucket;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,28 +33,6 @@ class S3Config {
   private String thumbnailsBucket;
 
   @Bean
-  @ConditionalOnProperty(
-      name="sandbox.s3.local-enabled",
-      havingValue = "true")
-  AmazonS3 s3ClientHttp() {
-    AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-    ClientConfiguration clientConfig = new ClientConfiguration();
-    clientConfig.setProtocol(Protocol.HTTP);
-    return AmazonS3ClientBuilder
-        .standard()
-        .withCredentials(new AWSStaticCredentialsProvider(credentials))
-        .withPathStyleAccessEnabled(true)
-        .withClientConfiguration(clientConfig)
-        .withEndpointConfiguration(
-            new EndpointConfiguration(endpoint, signingRegion))
-        .build();
-  }
-
-  @Bean
-  @ConditionalOnProperty(
-      name="sandbox.s3.local-enabled",
-      havingValue = "false",
-      matchIfMissing = true)
   AmazonS3 s3ClientHttps() {
     AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
     return AmazonS3ClientBuilder
