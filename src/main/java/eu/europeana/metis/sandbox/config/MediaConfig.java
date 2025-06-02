deba5bc50342263fb3a10356ccdb393ed5949219
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-06-02 15:06:41 +0200

    MET-6550 Media configuration in its own config class

---
+++ src/main/java/eu/europeana/metis/sandbox/config/MediaConfig.java
@@ -0,0 +1,29 @@
+package eu.europeana.metis.sandbox.config;
+
+import eu.europeana.metis.mediaprocessing.MediaExtractor;
+import eu.europeana.metis.mediaprocessing.MediaProcessorFactory;
+import eu.europeana.metis.mediaprocessing.RdfConverterFactory;
+import eu.europeana.metis.mediaprocessing.RdfDeserializer;
+import eu.europeana.metis.mediaprocessing.RdfSerializer;
+import eu.europeana.metis.mediaprocessing.exception.MediaProcessorException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MediaConfig {
+
+  @Bean
+  MediaExtractor mediaExtractor() throws MediaProcessorException {
+    return new MediaProcessorFactory().createMediaExtractor();
+  }
+
+  @Bean
+  RdfSerializer rdfSerializer() {
+    return new RdfConverterFactory().createRdfSerializer();
+  }
+
+  @Bean
+  RdfDeserializer rdfDeserializer() {
+    return new RdfConverterFactory().createRdfDeserializer();
+  }
+}
--- src/main/java/eu/europeana/metis/sandbox/config/SandboxConfig.java
+++ src/main/java/eu/europeana/metis/sandbox/config/SandboxConfig.java
@@ -9,33 +9,20 @@ import eu.europeana.enrichment.rest.client.exceptions.EnrichmentException;
 import eu.europeana.metis.harvesting.HarvesterFactory;
 import eu.europeana.metis.harvesting.http.HttpHarvester;
 import eu.europeana.metis.harvesting.oaipmh.OaiHarvester;
-import eu.europeana.metis.mediaprocessing.MediaExtractor;
-import eu.europeana.metis.mediaprocessing.MediaProcessorFactory;
-import eu.europeana.metis.mediaprocessing.RdfConverterFactory;
-import eu.europeana.metis.mediaprocessing.RdfDeserializer;
-import eu.europeana.metis.mediaprocessing.RdfSerializer;
-import eu.europeana.metis.mediaprocessing.exception.MediaProcessorException;
 import eu.europeana.metis.sandbox.repository.TransformXsltRepository;
 import eu.europeana.metis.sandbox.service.util.XsltUrlUpdateService;
 import eu.europeana.metis.sandbox.service.util.XsltUrlUpdateServiceImpl;
 import eu.europeana.metis.utils.apm.ElasticAPMConfiguration;
 import eu.europeana.normalization.NormalizerFactory;
-import java.lang.invoke.MethodHandles;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Redirect;
 import java.net.http.HttpClient.Version;
 import java.time.Duration;
-import java.util.concurrent.Executor;
-import javax.xml.xpath.XPathFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.integration.support.locks.LockRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -44,17 +31,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @ComponentScan("eu.europeana.validation.service")
 @EnableConfigurationProperties({ElasticAPMConfiguration.class})
 class SandboxConfig {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-    @Value("${sandbox.dataset.creation.threads.core-pool-size}")
-    private Integer corePoolSize;
-
-    @Value("${sandbox.dataset.creation.threads.max-pool-size}")
-    private Integer maxPoolSize;
-
-    @Value("${sandbox.dataset.creation.threads.thread-prefix}")
-    private String threadPrefix;
 
     @Value("${sandbox.enrichment.dereference-url}")
     private String dereferenceServiceUrl;
@@ -74,6 +50,11 @@ class SandboxConfig {
     @Value("${sandbox.portal.publish.record-base-url}")
     private String portalPublishRecordBaseUrl;
 
+    @Bean(name = "portalPublishRecordBaseUrl")
+    String portalPublishRecordBaseUrl() {
+        return portalPublishRecordBaseUrl;
+    }
+
     @Bean(name = "pipelineTaskExecutor")
     public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
@@ -86,30 +67,9 @@ class SandboxConfig {
     }
 
     @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    XPathFactory xPathFactory() {
-        return XPathFactory.newDefaultInstance();
-    }
-
-    @Bean
     XsltUrlUpdateService xsltUrlUpdateService(TransformXsltRepository transformXsltRepository,
                                               LockRegistry lockRegistry, HttpClient httpClient) {
         return new XsltUrlUpdateServiceImpl(transformXsltRepository, lockRegistry, httpClient);
-    }
-
-    @Bean
-    Executor asyncServiceTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaxPoolSize(maxPoolSize);
-        executor.setThreadNamePrefix(threadPrefix);
-        executor.initialize();
-        return executor;
-    }
-
-    @Bean(name = "portalPublishRecordBaseUrl")
-    String portalPublishRecordBaseUrl() {
-        return portalPublishRecordBaseUrl;
     }
 
     @Bean
@@ -138,42 +98,10 @@ class SandboxConfig {
     }
 
     @Bean
-    RdfConverterFactory rdfConverterFactory() {
-        return new RdfConverterFactory();
-    }
-
-    @Bean
-    MediaProcessorFactory mediaProcessorFactory() {
-        return new MediaProcessorFactory();
-    }
-
-    @Bean
-    MediaExtractor mediaExtractor() {
-        try {
-            return mediaProcessorFactory().createMediaExtractor();
-        } catch (MediaProcessorException mediaProcessorException) {
-            LOGGER.error("Unable to create media extractor", mediaProcessorException);
-            return null;
-        }
-    }
-
-    @Bean
-    RdfSerializer rdfSerializer() {
-        return rdfConverterFactory().createRdfSerializer();
-    }
-
-    @Bean
-    RdfDeserializer rdfDeserializer() {
-        return rdfConverterFactory().createRdfDeserializer();
-    }
-
-    @Bean
     HttpClient httpClient() {
         return HttpClient.newBuilder().version(Version.HTTP_2)
                 .followRedirects(Redirect.NORMAL)
                 .connectTimeout(Duration.ofSeconds(5))
                 .build();
     }
-
-
 }
