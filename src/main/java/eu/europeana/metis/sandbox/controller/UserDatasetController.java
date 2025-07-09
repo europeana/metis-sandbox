94acbf520d88d7685fed8521015f1ef4a3228994
andyjmaclean <andyjmaclean@gmail.com>
2025-07-09 15:33:12 +0200

    MET-6671 User Dataset Endpoint

---
+++ src/main/java/eu/europeana/metis/sandbox/controller/UserDatasetController.java
@@ -0,0 +1,73 @@
+package eu.europeana.metis.sandbox.controller;
+
+import static eu.europeana.metis.security.AuthenticationUtils.getUserId;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+
+import eu.europeana.metis.sandbox.dto.report.ProgressInfoDto;
+import eu.europeana.metis.sandbox.service.dataset.DatasetLogService;
+import eu.europeana.metis.sandbox.service.dataset.DatasetReportService;
+import eu.europeana.metis.sandbox.service.dataset.DatasetService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * The type User Dataset controller.
+ */
+@RestController
+@RequestMapping("/user-datasets")
+@Tag(name = "User Dataset Controller")
+class UserDatasetController {
+
+  private final DatasetService datasetService;
+  private final DatasetLogService datasetLogService;
+  private final DatasetReportService reportService;
+
+    @Autowired
+    public UserDatasetController(
+      DatasetService datasetService,
+      DatasetLogService datasetLogService,
+      DatasetReportService reportService
+     ) {
+        this.datasetService = datasetService;
+        this.datasetLogService = datasetLogService;
+        this.reportService = reportService;
+    }
+
+    //@Operation(summary = "Get user's datasets", description = "Get user's datasets"),
+    @ApiResponse(responseCode = "200", description = "Success")
+    @ApiResponse(responseCode = "400", description = "Error")
+    @GetMapping(value = "{id}", produces = APPLICATION_JSON_VALUE)
+    public ProgressInfoDto getDatasets(
+      @AuthenticationPrincipal Jwt jwtPrincipal
+    )
+    {
+        final String userId;
+        if (jwtPrincipal == null) {
+          userId = null;
+        } else {
+          userId = getUserId(jwtPrincipal);
+        }
+        // TODO: I want to change this to return an array of my new type
+        return this.reportService.getReport(userId);
+    }
+
+}
