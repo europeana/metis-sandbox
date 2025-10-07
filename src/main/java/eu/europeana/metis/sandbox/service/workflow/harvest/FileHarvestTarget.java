4c60a0836315076cbb5487b1e6eaadb6bb1b6d4f
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-10-07 14:02:54 +0200

    MET-6810 Process review

--- src/main/java/eu/europeana/metis/sandbox/service/workflow/harvest/FileHarvestTarget.java
+++ src/main/java/eu/europeana/metis/sandbox/service/workflow/harvest/FileHarvestTarget.java
@@ -1,7 +1,6 @@
 package eu.europeana.metis.sandbox.service.workflow.harvest;
 
 import eu.europeana.metis.sandbox.common.FileType;
-import lombok.Value;
 
 /**
  * Represents a target file to be harvested in the system.
@@ -9,10 +8,7 @@ import lombok.Value;
  * The FileHarvestTarget class encapsulates the metadata and content of a file to be used for harvesting operations. It includes
  * the file's name, type, and raw byte content.
  */
-@Value
-public final class FileHarvestTarget {
+@SuppressWarnings("java:S6218") // We don't want to expose the file content for equality.
+public record FileHarvestTarget(String fileName, FileType fileType, byte[] fileContent) {
 
-  String fileName;
-  FileType fileType;
-  byte[] fileContent;
 }
