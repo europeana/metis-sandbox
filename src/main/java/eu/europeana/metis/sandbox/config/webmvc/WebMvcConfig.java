dab55b0a8fe3d47f75c52ee170c9d64100cb1938
Simon Tzanakis <simon.tzanakis@europeana.eu>
2026-01-15 15:29:39 +0100

    MET-6557 Process review

--- src/main/java/eu/europeana/metis/sandbox/config/webmvc/WebMvcConfig.java
+++ src/main/java/eu/europeana/metis/sandbox/config/webmvc/WebMvcConfig.java
@@ -2,7 +2,6 @@ package eu.europeana.metis.sandbox.config.webmvc;
 
 import eu.europeana.metis.sandbox.controller.ratelimit.RateLimitInterceptor;
 import org.apache.commons.lang3.ArrayUtils;
-import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -45,7 +44,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   }
 
   @Override
-  public void addCorsMappings(@NonNull CorsRegistry registry) {
+  public void addCorsMappings(CorsRegistry registry) {
     if (ArrayUtils.isNotEmpty(corsMapping)) {
       registry.addMapping("/**").allowedOrigins(corsMapping)
               .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS");
