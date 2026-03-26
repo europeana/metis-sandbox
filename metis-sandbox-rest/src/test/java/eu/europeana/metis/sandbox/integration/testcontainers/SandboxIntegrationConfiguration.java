f48a4dbbff28ba0b32d302ef3f66a38ac7eb44b8
Simon Tzanakis <simon.tzanakis@europeana.eu>
2026-03-26 11:37:54 +0100

    MET-7054 Use adobe s3mock for testcontainers

--- metis-sandbox-rest/src/test/java/eu/europeana/metis/sandbox/integration/testcontainers/SandboxIntegrationConfiguration.java
+++ metis-sandbox-rest/src/test/java/eu/europeana/metis/sandbox/integration/testcontainers/SandboxIntegrationConfiguration.java
@@ -1,10 +1,13 @@
 package eu.europeana.metis.sandbox.integration.testcontainers;
 
+import static eu.europeana.metis.sandbox.integration.testcontainers.S3TestContainersConfiguration.ACCESS_KEY;
+import static eu.europeana.metis.sandbox.integration.testcontainers.S3TestContainersConfiguration.REGION;
+import static eu.europeana.metis.sandbox.integration.testcontainers.S3TestContainersConfiguration.SECRET_KEY;
+import static eu.europeana.metis.sandbox.integration.testcontainers.S3TestContainersConfiguration.getEndpoint;
+
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.localstack.LocalStackContainer;
-import org.testcontainers.containers.localstack.LocalStackContainer.Service;
 
 /**
  * Helper class for setting dynamic properties for Testcontainers.
@@ -47,16 +50,17 @@ public final class SandboxIntegrationConfiguration {
 
   private static void testContainersSolrConfiguraiton() {
     SolrTestContainersConfiguration.setDynamicProperty("sandbox.publish.solr.hosts",
-        container -> String.format("http://%s:%d/solr/%s", container.getHost(), container.getSolrPort(), SolrTestContainersConfiguration.SOLR_COLLECTION_NAME));
+        container -> String.format("http://%s:%d/solr/%s", container.getHost(), container.getSolrPort(),
+            SolrTestContainersConfiguration.SOLR_COLLECTION_NAME));
   }
 
   private static void testContainersS3Configuration() {
-    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.access-key", LocalStackContainer::getAccessKey);
-    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.secret-key", LocalStackContainer::getSecretKey);
-    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.endpoint",
-        container -> container.getEndpointOverride(Service.S3).toString());
-    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.signing-region", LocalStackContainer::getRegion);
-    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.thumbnails-bucket", container -> S3TestContainersConfiguration.BUCKET_NAME);
+    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.access-key", container -> ACCESS_KEY);
+    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.secret-key", container -> SECRET_KEY);
+    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.endpoint", container -> getEndpoint());
+    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.signing-region", container -> REGION);
+    S3TestContainersConfiguration.setDynamicProperty("sandbox.s3.thumbnails-bucket",
+        container -> S3TestContainersConfiguration.BUCKET_NAME);
   }
 
 }
